493942cd86a8167319a626f1f8acecd9a2172aec
n7rc <notrec@hotmail.com>
2018-04-30 11:27:32 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
 
-// Pendiente: definir un front-end diferente.
-
 require_once ("config.inc.php");
 require_once ("functions.inc.php");
 
@@ -11,7 +9,7 @@ mysqli_select_db($connection, $mysqldb);
 $date   = date('Y-m-d');
 $time   = date('H:i');
 $dateEnd = date_create($date);
-date_add ($dateEnd, date_interval_create_from_date_string('' .$dateRange . 'days'));
+date_add ($dateEnd, date_interval_create_from_date_string('' . $dateRange . 'days'));
 $dateEnd = date_format($dateEnd, 'Y-m-d');
 
 echo '<!DOCTYPE html>
@@ -26,20 +24,20 @@ echo '<!DOCTYPE html>
 			</head>
 			<body>';
 
-echo '<div id="pricing-table" class="clear">
-			<div class="plan" id="most-popular">
+echo '<div id="booking-table">
+			<div class="bookings" id="main">
 			<h3>' . $title;
 
 if (occupationState(qryState($date, $dateEnd, $mrbsType)) == 1){
-	echo '<span class="large"><font color="#4267b2">PRÓXIMAMENTE</font></span></h3>';
+  echo '<span class="status"><font color="#4267b2">PRÓXIMAMENTE</font></span></h3>';
+	printOccupationTable(qry($date, $dateEnd, $mrbsType));
+	echo '</div></div>';
+	getIndicators(qry($date, $dateEnd, $mrbsType));
 
 }else{
-	echo '<span class="large"><font color="#4267b2">ESTAMOS ESPERANDO TU TFE</font>
-				</span></h3>';
-
-}
-printOccupationTable(qry($date, $dateEnd, $mrbsType));
-echo '</div></div>';
-getIndicators(qry($date, $dateEnd, $mrbsType));
+	echo '<span class="status"><font color="#4267b2">ESTAMOS ESPERANDO TU TFE</font>
+				</span></h3>
+				</div></div>';
+	}
 echo '</body></html>';
 ?>
